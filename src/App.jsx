7602a905b5ffa193507e51707812a4bdbf4bec14
Dennis Mwangi <denniswachera@yahoo.com>
2024-12-11 15:04:58 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+   <div>
+   <p>Hello! there my name is Dennis, I am the best programmer.</p>
+   <button onClick={() => console.log("I am a child of God")}>
+    I am awesome
+      </button>
+    </div>
+  )
+ }
+
+export default App
